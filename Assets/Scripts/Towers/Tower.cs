d4c80c36c86d356bc1f08b0c23922e5d6ce97810
Emil Sunesson <sunesson.emil@gmail.com>
2022-04-02 23:12:10 +0200

    tower targeting

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    
+	public GameObject bullet;
+	public float reloadSpeed = 0.3f;
+	private float reload = 0f;
+	private TargetSeeker targetSeeker;
+	private void Awake() {
+		targetSeeker = GetComponentInChildren<TargetSeeker>();
+	}
 
+	public void Update() {
+
+		if (reload > 0) {
+			reload -= Time.deltaTime;
+		}
+
+		if (targetSeeker.GetTarget() != null) {
+			if (reload <= 0f) {
+				reload = reloadSpeed;
+				Fire();
+			}
+		}
+	}
+
+	private void Fire() {
+		var proj = Instantiate(bullet, transform.localPosition, transform.rotation);
+		proj.GetComponent<Projectile>().SetTarget(targetSeeker.GetTarget());
+	}
 
 }
