01baa9df9568e7b12df18e9e769d6e5ed7497a62
Adam Olofsson <catamstonecipher@gmail.com>
2022-04-04 18:59:35 +0200

    add theme

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
+    public AudioSource ThemeSongSource;
     public static Score score = new Score { banished = 0, wave = 0 };
     public bool started;
     private int money = 250;
@@ -66,7 +67,30 @@ public class GameManager : MonoBehaviour {
 
     public void SetWave(int wave) {
         this.wave = wave;
+        if (wave == 1)
+        {
+            StartCoroutine(PlayTheme());
+        }
         UIManager.instance.SetWave(wave.ToString());
+    }
+
+    public IEnumerator PlayTheme()
+    {
+        ThemeSongSource.volume = 0.0f;
+        ThemeSongSource.loop = true;
+        yield return new WaitForSeconds(3f);
+        ThemeSongSource.Play();
+        StartCoroutine(ToneInMusic(0.1f));
+    }
+
+    private IEnumerator ToneInMusic(float currentVolume)
+    {
+        ThemeSongSource.volume = currentVolume;
+        yield return new WaitForSeconds(0.5f);
+        if (currentVolume < 1.0f)
+        {
+            StartCoroutine(ToneInMusic(currentVolume + 0.1f));
+        }
     }
 
     public int Wave()
@@ -86,4 +110,5 @@ public class GameManager : MonoBehaviour {
         UnityEngine.SceneManagement.SceneManager.LoadScene("end");
 
     }
+
 }
